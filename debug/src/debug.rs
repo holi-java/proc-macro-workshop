bad342ff6b3395f3f29272469e02bb5d97d35543
feexon <feexon@foxmail.com>
2023-09-05 19:56:20 +0800

    pass: impl debug

---
+++ debug/src/debug.rs
@@ -0,0 +1,104 @@
+use proc_macro2::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{parse::Parse, Data, DeriveInput, Ident};
+
+pub(crate) struct Debug {
+    input: DeriveInput,
+}
+
+impl Debug {
+    fn name(&self) -> &Ident {
+        &self.input.ident
+    }
+}
+
+impl Parse for Debug {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let input = input.parse::<DeriveInput>()?;
+        Ok(Debug { input })
+    }
+}
+
+impl ToTokens for Debug {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let name = self.name();
+        let fmt: TokenStream = quote!(::core::fmt);
+        let fields = match self.input.data {
+            Data::Struct(ref s) => &s.fields,
+            _ => todo!(),
+        };
+
+        let debug_fields = fields.into_iter().map(|f| {
+            let name = &f.ident;
+            quote!(field(&stringify!(#name), &self.#name))
+        });
+        tokens.extend(quote!(
+            impl #fmt::Debug for Field {
+                fn fmt(&self, f: &mut #fmt::Formatter<'_>) -> #fmt::Result {
+                    f.debug_struct(stringify!(#name)).
+                        #(#debug_fields.)*
+                        finish()
+                }
+            }
+        ));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::assert_token_stream_eq;
+
+    macro_rules! debug {
+        ($($tt:tt)*) => {{
+            let debug: super::Debug = syn::parse_quote!($($tt)*);
+            debug
+        }};
+    }
+
+    #[test]
+    fn name() {
+        let debug = debug!(
+            #[derive(CustomDebug)]
+            struct Field;
+        );
+
+        assert_token_stream_eq!(debug.name(), { Field });
+    }
+
+    #[test]
+    fn impl_debug_for_unit_struct() {
+        let debug = debug!(
+            #[derive(CustomDebug)]
+            struct Field;
+        );
+
+        assert_token_stream_eq!(debug, {
+            impl ::core::fmt::Debug for Field {
+                fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+                    f.debug_struct(stringify!(Field)).finish()
+                }
+            }
+        });
+    }
+
+    #[test]
+    fn impl_debug_for_named_struct() {
+        let debug = debug!(
+            #[derive(CustomDebug)]
+            struct Field {
+                name: String,
+                readonly: bool,
+            }
+        );
+        assert_token_stream_eq!(debug, {
+            impl ::core::fmt::Debug for Field {
+                fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+                    f.debug_struct(stringify!(Field))
+                        .field(&stringify!(name), &self.name)
+                        .field(&stringify!(readonly), &self.readonly)
+                        .finish()
+                }
+            }
+        });
+    }
+}
--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,14 @@
 use proc_macro::TokenStream;
+mod debug;
+use debug::Debug;
+use quote::ToTokens;
+#[cfg(test)]
+use syn_test::assert_token_stream_eq;
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    TokenStream::new()
+    syn::parse::<Debug>(input)
+        .unwrap()
+        .into_token_stream()
+        .into()
 }
