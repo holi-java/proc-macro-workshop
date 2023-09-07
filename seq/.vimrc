d0486c302509569d9076ecc7233b6fab4dd53c51
feexon <feexon@foxmail.com>
2023-09-07 09:50:31 +0800

    pass: parse

---
+++ seq/.vimrc
@@ -0,0 +1,1 @@
+map <S-F10> :wa \| bel term cargo test --lib<CR>
--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -3,6 +3,5 @@ use proc_macro::TokenStream;
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
     let _ = input;
-
-    unimplemented!()
+    TokenStream::new()
 }
