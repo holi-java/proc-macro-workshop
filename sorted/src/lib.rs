89a39c50b1ea0e274e99a463af8d6c214e174cb6
feexon <feexon@foxmail.com>
2023-09-11 23:51:10 +0800

    pass: not enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,8 +1,14 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
+use syn::{Error, ItemEnum};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
+    if let Err(_) = syn::parse::<ItemEnum>(input) {
+        return Error::new(Span::call_site(), "expected enum or match expression")
+            .into_compile_error()
+            .into();
+    }
     TokenStream::new()
 }
